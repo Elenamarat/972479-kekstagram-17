406f1552ebfcd475357ef9d38e6be2a8ceacedee
unknown <maratelenauma@yandex.ru>
2019-07-01 14:25:48 +0500

    Личный проект: пока все дома

--- js/main.js
+++ js/main.js
@@ -2,17 +2,30 @@
 
 var photos = [];
 var comments = [];
+var urls = [];
 var notes = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var numberPhotos = 25;
+var numberLikes = 200;
 var numberComments = 3;
 
 var getRandom = function (min, max) {
   return Math.floor(Math.random() * ((max + 1) - min)) + min;
 };
 
-var getComments = function () {
-  for (var i = 0; i < numberComments; i++) {
+var getUrl = function (number) {
+  for (var i = 0; i < number; i++) {
+    urls[i] = i + 1;
+  }
+  urls.sort(function () {
+    return 0.5 - Math.random();
+  });
+
+  return urls;
+};
+
+var getComments = function (number) {
+  for (var i = 0; i < number; i++) {
     comments[i] = {
       avatar: 'img/avatar-' + getRandom(1, 6) + '.svg',
       message: notes[getRandom(0, notes.length - 1)],
@@ -23,37 +36,39 @@ var getComments = function () {
   return comments;
 };
 
-var getPhotos = function () {
-  for (var i = 0; i <= numberPhotos; i++) {
+var getPhotos = function (number) {
+  for (var i = 0; i < number; i++) {
     photos[i] = {
-      url: 'photos/' + getRandom(1, 5) + '.jpg',
-      likes: String(getRandom(15, 200)),
-      comments: getComments()
+      url: 'photos/' + urls[i] + '.jpg',
+      likes: getRandom(15, numberLikes),
+      comments: getComments(getRandom(1, numberComments))
     };
   }
 
   return photos;
 };
 
+getUrl(numberPhotos);
+getPhotos(numberPhotos);
+
 var pictureOtherUsers = document.querySelector('.pictures');
 
 var pictureUserTemplate = document.querySelector('#picture')
     .content
-    .querySelector('picture');
+    .querySelector('.picture');
 
-var renderPicture = function () {
+var renderPicture = function (photo) {
   var pictureElement = pictureUserTemplate.cloneNode(true);
-  getPhotos();
 
-  pictureElement.querySelector('.picture__img src').textContent = photos.url;
-  pictureElement.querySelector('.picture__likes').textContent = photos.likes;
-  pictureElement.querySelector('.picture__comments').textContent = photos.comments;
+  pictureElement.querySelector('.picture__img').setAttribute('src', photo.url);
+  pictureElement.querySelector('.picture__likes').textContent = photo.likes;
+  pictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
 
   return pictureElement;
 };
 
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < photos.length; i++) {
+for (var i = 0; i < numberPhotos; i++) {
   fragment.appendChild(renderPicture(photos[i]));
 }
 
