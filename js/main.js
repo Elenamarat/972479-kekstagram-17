32364ebc8fdfea1babb83d6d6e3e9f9de0c8bf1d
unknown <maratelenauma@yandex.ru>
2019-07-02 08:36:04 +0500

    Личный проект

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 'use strict';
 
-var photos = [];
-var comments = [];
-var urls = [];
-var notes = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var NOTES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var numberPhotos = 25;
 var numberLikes = 200;
 var numberComments = 3;
@@ -14,6 +11,7 @@ var getRandom = function (min, max) {
 };
 
 var getUrl = function (number) {
+  var urls = [];
   for (var i = 0; i < number; i++) {
     urls[i] = i + 1;
   }
@@ -25,12 +23,12 @@ var getUrl = function (number) {
 };
 
 var getComments = function (number) {
-  comments = [];
+  var comments = [];
   for (var i = 0; i < number; i++) {
     comments[i] = {
       avatar: 'img/avatar-' + getRandom(1, 6) + '.svg',
-      message: notes[getRandom(0, notes.length - 1)],
-      name: names[getRandom(0, names.length - 1)]
+      message: NOTES[getRandom(0, NOTES.length - 1)],
+      name: NAMES[getRandom(0, NAMES.length - 1)]
     };
   }
 
@@ -38,6 +36,7 @@ var getComments = function (number) {
 };
 
 var getPhotos = function (number) {
+  var photos = [];
   for (var i = 0; i < number; i++) {
     photos[i] = {
       url: 'photos/' + urls[i] + '.jpg',
@@ -49,8 +48,8 @@ var getPhotos = function (number) {
   return photos;
 };
 
-getUrl(numberPhotos);
-getPhotos(numberPhotos);
+var urls = getUrl(numberPhotos);
+var photos = getPhotos(numberPhotos);
 
 var pictureOtherUsers = document.querySelector('.pictures');
 
